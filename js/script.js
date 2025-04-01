05152f62d14a5b60f8b689fecafe290b73957ab6
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-04-01 13:28:33 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Ain Jeong All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ain Jeong
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates the volume of a sphere.
+ */
+function calculateVolumeOfSphere() {
+  // input
+  const radiusOfSphere = parseFloat(
+    document.getElementById("radius-of-sphere").value
+  )
+
+  // process
+  const volumeOfSphere = (4 / 3) * Math.PI * radiusOfSphere ** 3
+
+  // output
+  document.getElementById("answer").innerHTML =
+    "Volume of a Sphere is: " + volumeOfSphere.toFixed(2) + " mm³"
+}
